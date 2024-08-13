2f6eaf79bd3d1878ff4bf98021d0221bccf52345
James Seibel <jeseibel@gondtc.com>
2024-08-12 21:47:58 -0500

    Add optional DhApiChunk validation for world gen

--- fabric/src/main/java/com/seibel/distanthorizons/fabric/testing/TestWorldGenerator.java
+++ fabric/src/main/java/com/seibel/distanthorizons/fabric/testing/TestWorldGenerator.java
@@ -11,16 +11,28 @@ import com.seibel.distanthorizons.api.objects.data.DhApiChunk;
 import com.seibel.distanthorizons.api.objects.data.DhApiTerrainDataPoint;
 import com.seibel.distanthorizons.common.wrappers.chunk.ChunkWrapper;
 import com.seibel.distanthorizons.common.wrappers.world.ServerLevelWrapper;
+import com.seibel.distanthorizons.core.config.Config;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.chunk.ChunkAccess;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TestWorldGenerator extends AbstractDhApiChunkWorldGenerator
 {
+	private static final int MAX_QUEUED_TASKS_PER_THREAD = 10;
+	
 	private final ServerLevel level;
 	private final IDhApiLevelWrapper levelWrapper;
+	
+	private final AtomicInteger genCountRef = new AtomicInteger(0);
+	
+	
+	
+	//=============//
+	// constructor //
+	//=============//
 	
 	public TestWorldGenerator(ServerLevel level)
 	{
@@ -29,12 +41,30 @@ public class TestWorldGenerator extends AbstractDhApiChunkWorldGenerator
 	}
 	
 	
+	
+	//============//
+	// properties //
+	//============//
+	
 	@Override
 	public EDhApiWorldGeneratorReturnType getReturnType() { return EDhApiWorldGeneratorReturnType.API_CHUNKS; }
 	
 	@Override
-	public boolean isBusy() { return false; }
+	public boolean isBusy()
+	{
+		int worldGenThreadCount = Math.max(Config.Client.Advanced.MultiThreading.numberOfWorldGenerationThreads.get(), 1);
+		int maxWorldGenTaskCount = worldGenThreadCount * MAX_QUEUED_TASKS_PER_THREAD;
+		return this.genCountRef.get() > maxWorldGenTaskCount;
+	}
 	
+	@Override 
+	public boolean runApiChunkValidation() { return true; }
+	
+	
+	
+	//==================//
+	// chunk generation //
+	//==================//
 	
 	@Override
 	public Object[] generateChunk(int chunkX, int chunkZ, EDhApiDistantGeneratorMode eDhApiDistantGeneratorMode)
@@ -46,43 +76,59 @@ public class TestWorldGenerator extends AbstractDhApiChunkWorldGenerator
 	@Override
 	public DhApiChunk generateApiChunk(int chunkPosX, int chunkPosZ, EDhApiDistantGeneratorMode generatorMode)
 	{
-		// this test is only validated for 1.18.2 and up 
-		// (and it is only needed when testing world gen overrides/API chunks, so it isn't normally needed)
-		#if MC_VER >= MC_1_18_2
-		ChunkAccess chunk = this.level.getChunk(chunkPosX, chunkPosZ);
-		ChunkWrapper chunkWrapper = new ChunkWrapper(chunk, null, null);
-		
-		int minBuildHeight = chunkWrapper.getMinBuildHeight();
-		int maxBuildHeight = chunkWrapper.getMaxBuildHeight();
-		
-		DhApiChunk apiChunk = DhApiChunk.create(chunkPosX, chunkPosZ, minBuildHeight, maxBuildHeight);
-		for (int x = 0; x < 16; x++)
+		try
 		{
-			for (int z = 0; z < 16; z++)
+			this.genCountRef.incrementAndGet();
+			
+			
+			// this test is only validated for 1.18.2 and up 
+			// (and it is only needed when testing world gen overrides/API chunks, so it isn't normally needed)
+			#if MC_VER >= MC_1_18_2
+			ChunkAccess chunk = this.level.getChunk(chunkPosX, chunkPosZ);
+			
+			
+			int minBuildHeight = this.level.getMinBuildHeight();
+			int maxBuildHeight = this.level.getMaxBuildHeight();
+			
+			DhApiChunk apiChunk = DhApiChunk.create(chunkPosX, chunkPosZ, minBuildHeight, maxBuildHeight);
+			for (int x = 0; x < 16; x++)
 			{
-				ArrayList<DhApiTerrainDataPoint> dataPoints = new ArrayList<>();
-				
-				IDhApiBlockStateWrapper block = null;
-				IDhApiBiomeWrapper biome = null;
-				
-				for (int y = minBuildHeight; y < maxBuildHeight; y++)
+				for (int z = 0; z < 16; z++)
 				{
-					block = DhApi.Delayed.wrapperFactory.getBlockStateWrapper(new Object[]{chunk.getBlockState(new BlockPos(x, y, z))}, this.levelWrapper);
-					biome = DhApi.Delayed.wrapperFactory.getBiomeWrapper(new Object[]{chunk.getNoiseBiome(x, y, z)}, this.levelWrapper);
-					dataPoints.add(DhApiTerrainDataPoint.create((byte) 0, 0, 15, y, y + 1, block, biome));
+					ArrayList<DhApiTerrainDataPoint> dataPoints = new ArrayList<>();
+					
+					IDhApiBlockStateWrapper block = null;
+					IDhApiBiomeWrapper biome = null;
+					
+					for (int y = minBuildHeight; y < maxBuildHeight; y++)
+					{
+						block = DhApi.Delayed.wrapperFactory.getBlockStateWrapper(new Object[]{chunk.getBlockState(new BlockPos(x, y, z))}, this.levelWrapper);
+						biome = DhApi.Delayed.wrapperFactory.getBiomeWrapper(new Object[]{chunk.getNoiseBiome(x, y, z)}, this.levelWrapper);
+						dataPoints.add(DhApiTerrainDataPoint.create((byte) 0, 0, 15, y, y + 1, block, biome));
+					}
+					
+					apiChunk.setDataPoints(x, z, dataPoints);
 				}
-				
-				apiChunk.setDataPoints(x, z, dataPoints);
 			}
+			return apiChunk;
+			#else
+			return null;
+			#endif
 		}
-		return apiChunk;
-		#else
-		return null;
-		#endif
+		finally
+		{
+			this.genCountRef.decrementAndGet();
+		}
 	}
 	
 	@Override
 	public void preGeneratorTaskStart() { /* do nothing */ }
+	
+	
+	
+	//=========//
+	// cleanup //
+	//=========//
 	
 	@Override
 	public void close() { /* do nothing */ }
