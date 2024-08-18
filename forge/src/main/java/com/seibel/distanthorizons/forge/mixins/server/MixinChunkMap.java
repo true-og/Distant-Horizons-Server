16836a2b492a748c0df5acac7d8e10adeeb093d4
James Seibel <jeseibel@gondtc.com>
2024-08-18 14:46:49 -0500

    Move MixinChunkMap shared code to common

--- forge/src/main/java/com/seibel/distanthorizons/forge/mixins/server/MixinChunkMap.java
+++ forge/src/main/java/com/seibel/distanthorizons/forge/mixins/server/MixinChunkMap.java
@@ -1,8 +1,6 @@
 package com.seibel.distanthorizons.forge.mixins.server;
 
-import com.seibel.distanthorizons.common.wrappers.chunk.ChunkWrapper;
-import com.seibel.distanthorizons.common.wrappers.world.ServerLevelWrapper;
-import com.seibel.distanthorizons.core.api.internal.ServerApi;
+import com.seibel.distanthorizons.common.commonMixins.MixinChunkMapCommon;
 import net.minecraft.server.level.ChunkMap;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.chunk.ChunkAccess;
@@ -32,65 +30,6 @@ public class MixinChunkMap
 	// don't need the chunk(s) before MC has finished saving them
 	@Inject(method = "save", at = @At(value = "RETURN", target = CHUNK_SERIALIZER_WRITE))
 	private void onChunkSave(ChunkAccess chunk, CallbackInfoReturnable<Boolean> ci)
-	{
-		// true means a chunk was saved to disk
-		if (ci.getReturnValue())
-		{
-			// TODO is this validation necessary since we are checking above if 
-			//  the callback return value should state if the chunk was actually saved or not?
-			//  Do we trust it to always be correct?
-			
-			//=====================================//
-			// corrupt/incomplete chunk validation //
-			//=====================================//
-			
-			// MC has a tendency to try saving incomplete or corrupted chunks (which show up as empty or black chunks)
-			// this logic should prevent that from happening
-			#if MC_VER == MC_1_16_5 || MC_VER == MC_1_17_1
-			if (chunk.isUnsaved() || chunk.getUpgradeData() != null || !chunk.isLightCorrect())
-			{
-				return;
-			}
-			#else
-			if (chunk.isUnsaved() || chunk.isUpgrading() || !chunk.isLightCorrect())
-			{
-				return;
-			}
-			#endif
-			
-			
-			//==================//
-			// biome validation //
-			//==================//
-			
-			// some chunks may be missing their biomes, which cause issues when attempting to save them
-			#if MC_VER == MC_1_16_5 || MC_VER == MC_1_17_1
-			if (chunk.getBiomes() == null)
-			{
-				return;
-			}
-			#else
-			try
-			{
-				// this will throw an exception if the biomes aren't set up
-				chunk.getNoiseBiome(0,0,0);
-			}
-			catch (Exception e)
-			{
-				return;
-			}
-			#endif
-			
-			
-			
-			if (!SharedApi.isChunkAtBlockPosAlreadyUpdating(chunk.getPos().getWorldPosition().getX(), chunk.getPos().getWorldPosition().getZ()) )
-			{
-				ServerApi.INSTANCE.serverChunkSaveEvent(
-						new ChunkWrapper(chunk, this.level, ServerLevelWrapper.getWrapper(this.level)),
-						ServerLevelWrapper.getWrapper(this.level)
-				);
-			}
-		}
-	}
+	{ MixinChunkMapCommon.onChunkSave(this.level, chunk, ci); }
 	
 }
