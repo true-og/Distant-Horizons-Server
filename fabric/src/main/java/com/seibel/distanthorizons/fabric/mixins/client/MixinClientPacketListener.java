c289f5d71743c38ff8e14360c863a0bc80e80169
James Seibel <jeseibel@gondtc.com>
2024-11-19 06:58:12 -0600

    minor format cleanup

--- fabric/src/main/java/com/seibel/distanthorizons/fabric/mixins/client/MixinClientPacketListener.java
+++ fabric/src/main/java/com/seibel/distanthorizons/fabric/mixins/client/MixinClientPacketListener.java
@@ -25,10 +25,11 @@ public class MixinClientPacketListener
 	private ClientLevel level;
 	
 	@Inject(method = "handleLogin", at = @At("RETURN"))
-	void onHandleLoginEnd(CallbackInfo ci) { 
+	void onHandleLoginEnd(CallbackInfo ci) 
+	{ 
 		ClientApi.INSTANCE.onClientOnlyConnected(); 
 		ClientApi.INSTANCE.clientLevelLoadEvent(ClientLevelWrapper.getWrapper(this.level, true));
-}
+	}
 	
 	#if MC_VER < MC_1_19_4
 	@Inject(method = "cleanup", at = @At("HEAD"))
