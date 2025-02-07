e0da9a0548dcdc9d0cc1b7ef69a9f86480ac46b3
Jim C K Flaten <jckf@jckf.no>
2025-02-07 23:55:27 +0100

    Protocol v10.

--- src/main/java/no/jckf/dhsupport/core/handler/PluginMessageHandler.java
+++ src/main/java/no/jckf/dhsupport/core/handler/PluginMessageHandler.java
@@ -38,7 +38,7 @@ public class PluginMessageHandler
 
     public final String pluginChannel = "distant_horizons:message";
 
-    public final int protocolVersion = 9;
+    public final int protocolVersion = 10;
 
     private EventBus<PluginMessage> eventBus;
 
