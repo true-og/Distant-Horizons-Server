307d8795d9fd4cd150331c1c9d9149186e717178
Jim C K Flaten <jckf@jckf.no>
2025-03-03 01:56:27 +0100

    There's no time like now.

--- src/main/java/no/jckf/dhsupport/core/dataobject/Lod.java
+++ src/main/java/no/jckf/dhsupport/core/dataobject/Lod.java
@@ -132,8 +132,10 @@ public class Lod extends DataObject
         encoder.writeBoolean(true); // Apply to parent
         encoder.writeBoolean(false); // Apply to children
 
-        encoder.writeLong(0); // TODO: Last modified TS.
-        encoder.writeLong(0); // TODO: Created TS.
+        long now = System.currentTimeMillis();
+
+        encoder.writeLong(now);
+        encoder.writeLong(now);
     }
 
     protected byte[] compress(byte[] uncompressedData)
